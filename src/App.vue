a8c5acce64a9f2be503d9830dc230091efb135c5
Nimmly <marko.v.popovic@gmail.com>
2019-06-06 15:04:08 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
   import SearchBar from "./components/SearchBar";
   import VideoList from "./components/VideoList";
   import VideoDetail from "./components/VideoDetail";
-  const API_KEY= "AIzaSyD4429jCXfJ8NJuB3ZHrah5SUk-oSem1-c";
+  const API_KEY= "";
 
   export default {
     name: "App",
